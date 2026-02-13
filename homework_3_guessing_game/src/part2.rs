7616f2195c3c3c419df2cd1aed6c6d2729c19676
sammi-sj <sammijew@bu.edu>
2026-02-12 22:42:48 -0500

    solution for part2

--- homework_3_guessing_game/src/part2.rs
+++ homework_3_guessing_game/src/part2.rs
@@ -7,6 +7,19 @@ pub struct Part2 {}
 impl Strategy for Part2 {
     fn guess_the_number(player: &mut Player, min: u32, max: u32) -> u32 {
         // YOUR SOLUTION GOES HERE.
-        todo!("No solution provided yet")
+        let guess = min + (max - min)/2;
+        let x: i32 = player.ask_to_compare(guess);
+        if x == 0 {
+            return guess;
+        }
+        else if x == 1 {
+            return Self::guess_the_number(player, guess, max);
+        }
+        else if x == -1 {
+            return Self::guess_the_number(player, min, guess);
+        }
+        else {
+            return 0
+        }
     }
 }
