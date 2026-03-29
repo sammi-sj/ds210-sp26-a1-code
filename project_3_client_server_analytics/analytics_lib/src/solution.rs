0b1db007db63d1145a225a3ff8945cf0f002ea56
sammi-sj <sammijew@bu.edu>
2026-03-29 17:18:44 -0400

    std1 solution

--- project_3_client_server_analytics/analytics_lib/src/solution.rs
+++ project_3_client_server_analytics/analytics_lib/src/solution.rs
@@ -2,7 +2,7 @@ use std::collections::HashMap;
 use crate::dataset::{ColumnType, Dataset, Value, Row};
 use crate::query::{Aggregation, Condition, Query};
 
-pub fn evaluate_condition(row: &Row, condition: &Condition, dataset: &Dataset) -> bool {
+pub fn evaluate_condition(row: &Row, condition: &Condition, dataset: &Dataset) -> bool { //helper function
     match condition {
         Condition::Equal(col_name, value) => {
             let col_index = dataset.column_index(col_name);
@@ -25,21 +25,16 @@ pub fn filter_dataset(dataset: &Dataset, filter: &Condition) -> Dataset {
     result
 }
 
-/*
-    let mut result = Dataset::new(dataset.columns().clone());
-    let column_to_check = dataset.column_index(&filter.get_group_by());
+pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> { //std1
+    //todo!("Implement this!");
+    let mut result: HashMap<Value, Dataset> = HashMap::new();
+    let col_index = dataset.column_index(group_by_column);
     for row in dataset.iter() {
-        let value_to_check = row.get_value(column_to_check);
-        if value_to_check == Condition::Equal(filter) {
-            result.add_row(row.clone());
-        }
+        result.entry(row.get_value(col_index).clone())
+              .or_insert(Dataset::new(dataset.columns().clone()))
+              .add_row(row.clone());
     }
     result
-}
-*/
-
-pub fn group_by_dataset(dataset: Dataset, group_by_column: &String) -> HashMap<Value, Dataset> {
-    todo!("Implement this!");
 }
 
 pub fn aggregate_dataset(dataset: HashMap<Value, Dataset>, aggregation: &Aggregation) -> HashMap<Value, Value> {
