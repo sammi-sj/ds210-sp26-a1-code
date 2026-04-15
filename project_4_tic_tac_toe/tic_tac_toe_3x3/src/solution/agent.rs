8d29db4d89f84f3a8d866fea0a3701e42993f439
sammi-sj <sammijew@bu.edu>
2026-04-14 20:29:14 -0400

    new branch

--- project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
+++ project_4_tic_tac_toe/tic_tac_toe_3x3/src/solution/agent.rs
@@ -11,29 +11,32 @@ impl Agent for SolutionAgent {
     // where <score> is your estimate for the score of the game
     // and <x>, <y> are the position of the move your solution will make.
     fn solve(board: &mut Board, player: Player, _time_limit: u64) -> (i32, usize, usize) {
+        
         if board.game_over() {
             return (board.score(), 0, 0);      
-    }
+        }
+        
         let mut best_move = (0,0);
-        let maximizing = player == Player::X;
-        let mut best_score = if maximizing {
-            i32::MIN 
-        } else {
+        let maximizing = player == Player::X; //true if player is X, false if player is O
+        let mut best_score = //best score is max if maximizing, min if minimizing
+            if maximizing {
+                i32::MIN 
+            }
+            else {
             i32::MAX
-        };
-
+            };
         
         for (x,y) in board.moves() {
             let mut new_board = board.clone();
             new_board.apply_move((x, y), player);
 
-            let next_player = match player {
-                Player::X => Player::O,
-                Player::O => Player::X,
-            };
+            let next_player = 
+                match player {
+                    Player::X => Player::O,
+                    Player::O => Player::X,
+                };
 
-            let (score, _, _) =
-                SolutionAgent::solve(&mut new_board, next_player, _time_limit);
+            let (score, _, _) = SolutionAgent::solve(&mut new_board, next_player, _time_limit);
 
             if maximizing && score > best_score {
                 best_score = score;
@@ -45,11 +48,11 @@ impl Agent for SolutionAgent {
                 best_move = (x, y);
             }    
         
-            }
-            return (best_score, best_move.0, best_move.1);
-
+        }
+    return (best_score, best_move.0, best_move.1);
     }
+}
         // If you want to make a recursive call to this solution, use
         // `SolutionAgent::solve(...)`
-    }
+
 
